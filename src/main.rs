3709ca12b8da7cbc4d9406a37846361f832c3c8e
Willow <willow.ch@protonmail.com>
2021-03-23 20:54:09 -0500

    Added multithreading (gosh that was really easy?)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::io::{stdin, BufRead};
 
 const WORDS_LIST: &'static str = include_str!("word_list.txt");
@@ -8,13 +9,14 @@ const CHARS: [char; 26] = [
 fn main() {
 	let words = WORDS_LIST
 		.lines()
+		.par_bridge()
 		.map(|v| v.to_string())
 		.collect::<Vec<String>>();
 	let mut guessed: Vec<char> = vec![];
 	println!("Type your word: ");
 	loop {
 		let word = stdin().lock().lines().next().unwrap().unwrap();
-		if word.chars().into_iter().filter(|v| v == &'_').count() == 0 {
+		if word.chars().par_bridge().filter(|v| v == &'_').count() == 0 {
 			if word.len() > 0 {
 				println!("I win!");
 			}
@@ -35,7 +37,7 @@ fn main() {
 fn best_letter(words: &Vec<String>, word: String, guessed: Vec<char>) -> Result<&char, bool> {
 	println!("\n");
 	let remaining_words = words
-		.into_iter()
+		.into_par_iter()
 		.filter(|v| {
 			if v.len() != word.len() {
 				return false;
@@ -78,11 +80,11 @@ fn best_letter(words: &Vec<String>, word: String, guessed: Vec<char>) -> Result<
 		println!("{} words are under consideration...", remaining_words.len());
 	}
 	let available = CHARS
-		.iter()
+		.par_iter()
 		.filter(|v| !guessed.contains(v))
 		.collect::<Vec<&char>>();
 	let mut counts = available
-		.into_iter()
+		.into_par_iter()
 		.map(|ch| {
 			(
 				ch,
@@ -109,29 +111,61 @@ fn check_every_word() {
 		.lines()
 		.map(|v| v.to_string())
 		.collect::<Vec<String>>();
-	for target in words.clone() {
-		let mut guessed: Vec<char> = vec![];
-		let correct: bool = {
-			let mut correct = false;
-			for _ in 0..26 {
-				let word = target
-					.chars()
-					.into_iter()
-					.map(|ch| if guessed.contains(&ch) { ch } else { '_' })
-					.collect::<String>();
-				let guess = best_letter(&words, word, guessed.clone());
-				match guess {
-					Ok(g) => {
-						guessed.push(g.clone());
-					}
-					Err(correct_) => {
-						correct = correct_;
-						break;
-					}
-				};
-			}
+	let successes = words
+		.clone()
+		.par_iter()
+		.map(|target| {
+			let mut guessed: Vec<char> = vec![];
+			let correct: bool = {
+				let mut correct = false;
+				for _ in 0..26 {
+					let word = target
+						.chars()
+						.into_iter()
+						.map(|ch| if guessed.contains(&ch) { ch } else { '_' })
+						.collect::<String>();
+					let guess = best_letter(&words, word, guessed.clone());
+					match guess {
+						Ok(g) => {
+							guessed.push(g.clone());
+						}
+						Err(correct_) => {
+							correct = correct_;
+							break;
+						}
+					};
+				}
+				correct
+			};
 			correct
-		};
-		assert!(correct);
+		})
+		.collect::<Vec<bool>>();
+	for i in successes {
+		assert!(i)
 	}
+	// for target in words.clone() {
+	// 	let mut guessed: Vec<char> = vec![];
+	// 	let correct: bool = {
+	// 		let mut correct = false;
+	// 		for _ in 0..26 {
+	// 			let word = target
+	// 				.chars()
+	// 				.into_iter()
+	// 				.map(|ch| if guessed.contains(&ch) { ch } else { '_' })
+	// 				.collect::<String>();
+	// 			let guess = best_letter(&words, word, guessed.clone());
+	// 			match guess {
+	// 				Ok(g) => {
+	// 					guessed.push(g.clone());
+	// 				}
+	// 				Err(correct_) => {
+	// 					correct = correct_;
+	// 					break;
+	// 				}
+	// 			};
+	// 		}
+	// 		correct
+	// 	};
+	// 	assert!(correct);
+	// }
 }
